77423261b5c51438335995ec2c4236f8c4c98d61
Adam <adamparuch4@gmail.com>
2025-10-16 19:50:45 -0400

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-alert("welcome to my blog it sometimes takes a lil bit for the site render");
-console.log("hello wolrd this is a js program and hey");
-console.log("why are you examineing my site hhhhhhmmmmmmmmmm?");
