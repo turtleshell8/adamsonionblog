1391447e7f826bd1d770bb5a84641c6fda26dd2e
Adam <adamparuch4@gmail.com>
2025-12-02 19:27:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("hello world");
 console.log("anyway seeing that your here i want to show you some extra");
 console.log("stuff i wanted to add but couldnt because itll make it ugly");
-console.log("https://www.calculatorsoup.com thats my fav calculator website");
+console.log("https://www.calculatorsoup.com thats my fav calculator website thats it");
