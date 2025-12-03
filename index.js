1d444f1fc5410189b3566f06116c1409c0f562ab
Adam <adamparuch4@gmail.com>
2025-12-02 19:43:59 -0500

    Add simple calculator functionality to index.js

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ console.log("hello world");
 console.log("anyway seeing that your here i want to show you some extra");
 console.log("stuff i wanted to add but couldnt because itll make it ugly");
 console.log("https://www.calculatorsoup.com thats my fav calculator website thats it");
+// creds to https://www.programiz.com/javascript/examples/simple-calculator for the calculator i aint gonna learn js
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+console.log(`${number1} ${operator} ${number2} = ${result}`);
