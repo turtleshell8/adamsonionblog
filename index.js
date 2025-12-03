2015107765f50c9e2da42e0b80fa9ab06db5fbc6
Adam <adamparuch4@gmail.com>
2025-12-02 19:47:26 -0500

    Add comment to index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("hello world");
 console.log("anyway seeing that your here i want to show you some extra");
 console.log("stuff i wanted to add but couldnt because itll make it ugly");
 console.log("https://www.calculatorsoup.com thats my fav calculator website thats it");
+// js comment
